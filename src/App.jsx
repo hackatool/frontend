bfc5564f757885c63c7eb79877c7193bd3c2c99f
lucaspontoexe <ltx16@hotmail.com>
2019-09-21 00:35:39 -0300

    move bolierplate to 'old' folder

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+        <p>
+          tem nada aqui (só backup)
+        </p>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;

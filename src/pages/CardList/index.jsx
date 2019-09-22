da2e94b7b7300258ebd58eaf87fb58996dc8436c
lucaspontoexe <ltx16@hotmail.com>
2019-09-22 02:12:44 -0300

    eita

---
+++ src/pages/CardList/index.jsx
@@ -0,0 +1,94 @@
+import React, { Component } from 'react';
+import { Link } from "react-router-dom"
+
+import Icon from './icon';
+
+import "./style.scss";
+
+// actually this is how we import the icons
+import menu from '../../assets/menu.svg';
+import logo from '../../assets/logo.svg';
+import search from '../../assets/search.png';
+import UserProfile from '../../components/UserProfile';
+import Menu from '../../components/Menu';
+
+
+export default class CardList extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            showMenu: false,
+            showSearch: false,
+        };
+    }
+
+    render() {
+        return (
+            <>
+                <nav>
+                    <img
+                        className="clickable"
+                        src={menu}
+                        alt="menu"
+                        onClick={() => { this.setState({ showMenu: true }) }} />
+                    <img
+                        src={logo}
+                        alt="logo" />
+                    <img
+                        className="clickable"
+                        src={search}
+                        alt="search" />
+                </nav>
+                {/* TODO: toggle menu function */}
+
+                {this.state.showMenu && <Menu onDismiss={ () => {this.setState({showMenu: false})} } />}
+
+                {/* POG (WOP) >>> GO HORSE */}
+                <div className="container-wrapper">
+                    <div className="container">
+
+                        <Link to="/user">
+                            <div className="card profile">
+                                <UserProfile></UserProfile>
+                            </div>
+                        </Link>
+
+                        <div className="card counter">
+
+                            <Icon number={319}></Icon>
+
+                            <span className="description">
+                                Hackathons participados
+                        </span>
+
+                        </div>
+
+                        <div className="card list hacks">
+                            <h2>Últimos Hackathons</h2>
+                            <div className="empty">
+                                <span>
+                                    Adicione já
+                            </span>
+                            </div>
+
+                            <Link to="/hackathons">
+                                mais>>>
+                            </Link>
+                        </div>
+
+                        <div className="card list projects">
+                            <h2>Últimos Times</h2>
+                            <div className="empty">
+                                <span>
+                                    Adicione seus times
+                            </span>
+                            </div>
+                            <a href="#mais">mais>>></a>
+                        </div>
+                    </div>
+                </div>
+            </>
+        );
+    }
+}

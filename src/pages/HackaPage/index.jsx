aa1100560e5a09fda5adeebfa7f0d8fcedc30db2
lucaspontoexe <ltx16@hotmail.com>
2019-09-21 23:45:46 -0300

    make HackaPage style modularized

--- src/pages/HackaPage/index.jsx
+++ src/pages/HackaPage/index.jsx
@@ -1,17 +1,17 @@
 import React, { Component } from 'react'
 import NavBar from '../../components/NavBar'
 
+import { list, empty } from './style.module.scss';
 
 export default class HackaPage extends Component {
     render() {
         return (
             <div>
-                {/* WIP: make navbar a component */}
-
                 <NavBar></NavBar>
 
-                <div className="card list2">
-                    <div className="empty">
+                {/* POG bom e bonito */}
+                <div className={['card', list ].join(' ')}>
+                    <div className={empty}>
                         <span>Nenhum encontrado</span>
                     </div>
                 </div>
